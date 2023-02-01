21e274d82d432872ca6130606785b5ac7f3c513f
Alex Ru <Alex.ru.tver@gmail.com>
2023-02-02 00:00:18 +0300

    1 вариант привет маша

---
+++ Project005_ПриветМаша/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Привет, введите ваше имя: ");  //Спрашиваем имя
+string username = Console.ReadLine();         //Создаём переменную для хранения введённого имени
+
+if(username == "Маша")                       
+{
+    Console.WriteLine("Ура, это же Маша");
+}
+else
+{
+    Console.WriteLine("Привет, " );
+    Console.WriteLine(username);
+}
