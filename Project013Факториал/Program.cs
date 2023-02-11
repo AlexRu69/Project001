9f311c820aa3ed86aba610b8d31c9b2c6050252d
Alex Ru <Alex.ru.tver@gmail.com>
2023-02-12 02:34:40 +0300

    4 лекция

---
+++ Project013Факториал/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+
+double Fac(int n)
+{
+    if (n == 1) return 1;
+    else return n * Fac(n - 1);
+}
+for (int i = 1; i < 40; i++)
+{
+Console.WriteLine(Fac(i));   
+}
---
+++ Project14Фибоначи/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+
+double Fibon(int n)
+{
+    if (n == 1 || n == 2) return 1;
+    else return Fibon(n - 1) + Fibon(n - 2);
+}
+for (int i = 1; i < 10; i++)
+{
+    Console.WriteLine(Fibon(i));   
+}
+
+//Console.WriteLine(Fibon(10));
